b001fb5c7e32202a95529b06858f762febb295c3
Shivam-6497-trigent <shivam_s@trigent.com>
2024-10-18 01:17:40 +0530

    url changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5001');
+const socket = io('https://ws-demo-backend.onrender.com');
 
 const App = () => {
   const [message, setMessage] = useState('');
